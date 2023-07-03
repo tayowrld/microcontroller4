cd3ac56411f1c93c050da80631fa1ecb181b098f
Ivan <127036325+tayowrld@users.noreply.github.com>
2023-07-03 17:16:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ var accept = true;
 
 function buy(){
     if (accept == true){
-        window.location.href = 'https://mastercpu.e-autopay.com/buy/522099';
+        window.location.href = 'http://mastercpu.e-autopay.com/buy/522099';
     }
     else{
         alert("Необходимо согласие");
